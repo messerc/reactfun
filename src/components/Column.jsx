af38afbcf6b025deae20b8c93c80540ec8df5573
messerc <messer55@gmail.com>
2017-09-30 08:52:28 -0700

    need to add nested drop targets as well as a sortable interface (card is both drag source and drop target)

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import update from 'immutability-helper';
 import PropTypes from 'prop-types';
 import { DropTarget, connectDropTarget } from 'react-dnd';
 import { ItemTypes } from './Constants';
@@ -22,14 +23,45 @@ function collect(connect, monitor) {
 }
 
 class Column extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: this.props.notes
+    }
+
+  }
+
+  moveCard = (dragIndex, hoverIndex) => {
+    const { notes } = this.state;
+    const dragCard = notes[dragIndex];
+
+    this.setState(update(this.state, {
+      notes: {
+        $splice: [
+          [dragIndex, 1],
+          [hoverIndex, 0, dragCard]
+        ]
+      }
+    }));
+  }
+
   render() {
-    const { connectDropTarget, isOver, notes, title } = this.props
+    const { connectDropTarget, isOver, title } = this.props;
+    const { notes } = this.state;
+
+    let backgroundColor = '#F3F1F1'
+
+    if (isOver) {
+      backgroundColor = 'red';
+    }
+
     return connectDropTarget(
       <div>
         <h6>{title}</h6>
         <hr />
-        <div className='col tasks p-0'>
-          {notes.map((note, i) => <KanbanNote key={i} {...note} />)}
+        <div className='col tasks p-0' style={{ backgroundColor }}>
+          {notes.map((note, i) => <KanbanNote key={i} index={i} moveCard={this.moveCard} {...note} />)}
         </div>
       </div>
     )
